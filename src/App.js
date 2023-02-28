e6268e2a8cfc3f94e73f72d558d56fcc5502ff7c
Moola Reddy <moolasreddy@yahoo.com>
2023-02-28 17:50:54 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import { AppProvider } from './context/AppContext';
-import Budget from './components/Budget';
-import ExpenseTotal from './components/ExpenseTotal';
+import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
-import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+import ItemSelected from './components/ItemSelected';
+import Location from './components/Location';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
-                    {/* Add Budget component here under */}                    
+                <h1 className='mt-3'>Shopping App</h1>
+                <div className='row mt-3'> 
                     <div className='col-sm'>
-                        <Budget />
+                        <CartValue />
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
-                    </div>
-                    <div className='col-sm'>
-                        <ExpenseTotal />
+                        <Location />
                     </div>
                 </div>
-                <h3 className='mt-3'>Allocation</h3>
+                <h3 className='mt-3'>Shopping Cart</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
-                <h3 className='mt-3'>Change allocation</h3>
+                <h3 className='mt-3'>Add Items</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <AllocationForm/>
+                        <ItemSelected/>
                     </div>
                 </div>
             </div>
